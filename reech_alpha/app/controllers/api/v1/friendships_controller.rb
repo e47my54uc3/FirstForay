04544172a9ecc8abba6b54458686f4af45e2236a
Ubuntu <ubuntu@ip-172-31-0-188.us-west-2.compute.internal>
2014-04-30 15:07:57 +0000

    Fixing issue in friendships api

--- reech_alpha/app/controllers/api/v1/friendships_controller.rb
+++ reech_alpha/app/controllers/api/v1/friendships_controller.rb
@@ -8,7 +8,7 @@ module Api
 				user = User.find_by_reecher_id(params[:user_id])
 				@friends = user.friendships.where(:status => "accepted")
 				@friends_list = []
-				@friends.select {|f| @friends_list << {:name => User.find_by_reecher_id(f.friend_reecher_id.strip).full_name, :email => User.find_by_reecher_id(f.friend_reecher_id).email} } if @friends.size > 0
+				@friends.select {|f| @friends_list << {:name => User.find_by_reecher_id(f.friend_reecher_id.strip).full_name, :email => User.find_by_reecher_id(f.friend_reecher_id.strip).email} } if @friends.size > 0
 				logger.debug "******Response To #{request.remote_ip} at #{Time.now} => #{ @friends_list }"
 				msg = {:status => 200, :friends_list => @friends_list }
 				render :json => msg

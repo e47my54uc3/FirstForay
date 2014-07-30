da01c3d964cb4d7e2cd80145c0b6e9cc7f3b52c1
Vijay Prasad <vijay@mailrelay.india.rsystems.com>
2014-07-30 19:51:12 +0530

    Undo all changes for Gemfile,Gemfile.lock,database,schema and seed files

--- reech_alpha/db/schema.rb
+++ reech_alpha/db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
 ActiveRecord::Schema.define(:version => 20140728130848) do
-
 
   create_table "api_keys", :force => true do |t|
     t.string   "access_token"

8b3b86a60de5efa1e89a45827a005ce9d0cd0c56
Vijay Prasad <vijay@mailrelay.india.rsystems.com>
2014-08-14 19:12:25 +0530

    Add s3 setting and SMTP config

--- reech_alpha/config/environments/test.rb
+++ reech_alpha/config/environments/test.rb
@@ -34,4 +34,36 @@ Reech::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+  
+# Do not compress assets
+  config.assets.compress = false
+
+  # Expands the lines which load the assets
+  config.assets.debug = true
+
+  #required by devise. Set host to correct host in production mode
+
+  config.action_mailer.default_url_options = { :host => 'ec2-54-201-116-44.us-west-2.compute.amazonaws.com:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            'hello@reechout.co',
+  password:             'Superhelper1!',
+  authentication:       'login',
+  enable_starttls_auto: true  }
+
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => 'reechattachmentstorage',
+    :access_key_id => 'AKIAIVK7XM7Q7YX72IDQ',
+    :secret_access_key => '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'
+    
+  },
+  :s3_multipart_min_part_size => 20971520
+ }
+  
+  
 end

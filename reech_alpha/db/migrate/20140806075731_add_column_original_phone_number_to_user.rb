011bcf05e7e63fa6801755bbcdcf40229b3895c0
Vijay Prasad <vijay@mailrelay.india.rsystems.com>
2014-08-14 13:08:00 +0530

    Fixed stating server issue for FB user

--- reech_alpha/db/migrate/20140806075731_add_column_original_phone_number_to_user.rb
+++ reech_alpha/db/migrate/20140806075731_add_column_original_phone_number_to_user.rb
@@ -1,5 +1,5 @@
 class AddColumnOriginalPhoneNumberToUser < ActiveRecord::Migration
   def change
-     add_column  :users, :original_phone_number, :string ,:after=>:phone_number,:limit=>15
+     add_column  :users, :original_phone_number, :string ,:after=>:phone_number,:limit=>20
   end
 end

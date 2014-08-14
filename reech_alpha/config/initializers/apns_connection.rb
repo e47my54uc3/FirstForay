97fdd60d463f87345419c4ef490edbcf4f85ba62
Vijay Prasad <vijay@mailrelay.india.rsystems.com>
2014-08-14 19:44:13 +0530

    Add new pam file for apple store

--- reech_alpha/config/initializers/apns_connection.rb
+++ reech_alpha/config/initializers/apns_connection.rb
@@ -5,7 +5,20 @@ APNS.host = 'gateway.sandbox.push.apple.com'
 APNS.port = 2195 
 # this is also the default. Shouldn't ever have to set this, but just in case Apple goes crazy, you can.
 
+if Rails.env.development?
 path = File.join(Rails.root, "config/apple_push_notification_development.pem")
+#http://reechattachmentstorage.s3.amazonaws.com/user_profiles/pictures/000/000/005/medium/data.txt?1407334787
+else
+path = File.join(Rails.root, "config/reech_pushcert.pem")
+end
+
+
+
+
+
+
+
+
 APNS.pem  = path
 
 #APNS.pass = ''

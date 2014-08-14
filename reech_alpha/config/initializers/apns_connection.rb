69bfeb3513c8d6cfd9c1ea5a02fb596e21d3f647
Ubuntu <ubuntu@ip-172-31-13-220.us-west-2.compute.internal>
2014-08-14 16:15:07 +0000

    Fixed conflict in paper clip in production

--- reech_alpha/config/initializers/apns_connection.rb
+++ reech_alpha/config/initializers/apns_connection.rb
@@ -1,17 +1,21 @@
-APNS.host = 'gateway.sandbox.push.apple.com' 
+#APNS.host = 'gateway.sandbox.push.apple.com' 
+APNS.host = 'gateway.push.apple.com' 
 # gateway.sandbox.push.apple.com is default and only for development
 # gateway.push.apple.com is only for production
 
 APNS.port = 2195 
 # this is also the default. Shouldn't ever have to set this, but just in case Apple goes crazy, you can.
 
+#path = File.join(Rails.root, "config/apple_push_notification_development.pem")
+path = File.join(Rails.root, "config/reech_pushcert.pem")
+=begin
 if Rails.env.development?
 path = File.join(Rails.root, "config/apple_push_notification_development.pem")
 #http://reechattachmentstorage.s3.amazonaws.com/user_profiles/pictures/000/000/005/medium/data.txt?1407334787
 else
 path = File.join(Rails.root, "config/reech_pushcert.pem")
 end
-
+=end
 
 
 
--- reech_alpha/config/initializers/paperclip.rb
+++ reech_alpha/config/initializers/paperclip.rb
@@ -3,7 +3,8 @@ if Rails.env.development?
  Paperclip::Attachment.default_options[:path] = '/development' + '/:class/:attachment/:id_partition/:style/:filename' 
 # http://reechattachmentstorage.s3.amazonaws.com/user_profiles/pictures/000/000/005/medium/data.txt?1407334787
 elsif Rails.env.test?
-Paperclip::Attachment.default_options[:path] = '/development'+ '/:class/:attachment/:id_partition/:style/:filename'
+Paperclip::Attachment.default_options[:path] = '/staging'+ '/:class/:attachment/:id_partition/:style/:filename'
 else
   Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 end
+

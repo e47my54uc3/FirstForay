6834889845168d62b0d9e3b483c16736831cff56
phani <phani@heresy.in>
2014-09-01 12:34:20 +0530

    refactor1 friendshipscontroller in api/vi

--- reech_alpha/app/controllers/api/v1/friendships_controller.rb
+++ reech_alpha/app/controllers/api/v1/friendships_controller.rb
@@ -4,36 +4,8 @@ module Api
 		before_filter :restrict_access
 		respond_to :json
 
-			def index
-				user = User.find_by_reecher_id(params[:user_id])
-				@friends = user.friendships.where(:status => "accepted")
-				@friends_list = []
-				if @friends.size >0
-  				@friends.each do |f| 
-          user= User.find_by_reecher_id(f.friend_reecher_id)  
-          group_ids = Group::get_friend_associated_groups params[:user_id] ,user.id
-          user_group_ids =[]
-          group_ids.each do |i|
-            user_group_ids.push(i.values)
-          end
-          user_group_ids.flatten! 
-          userProfile = user.user_profile if user
-          
-          if !userProfile.picture_file_name.blank?  
-          image_url = userProfile.picture_url 
-          else 
-          image_url =  userProfile.profile_pic_path 
-          end
-          userProfile.picture_file_name != nil ? userProfile[:image_url] =  userProfile.picture_url : userProfile[:image_url] = nil
-          @friends_list << {:name => user.first_name + " " + user.last_name,:email=> user.email,"reecherId" =>user.reecher_id,:location=>userProfile.location ,:image_url =>image_url,:associated_group_ids=>user_group_ids }
-          end 
-				end
-				
-				@friends_list=@friends_list.sort_by{|s| s[:name]}
-				groups=Group::reecher_personal_groups params[:user_id]
-				logger.debug "******Response To #{request.remote_ip} at #{Time.now} => #{ @friends_list }"
-				msg = {:status => 200, :friends_list => @friends_list,:groups =>groups }
-				render :json => msg
+			def index		
+				render "index.json.jbuilder"
 			end
       #END 
       

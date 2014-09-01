de0d677cc76dcc37b23b853757c085c8c873c33a
phani <phani@heresy.in>
2014-09-01 17:54:38 +0530

    removed index,new,update,show,destroy in groups controller

--- reech_alpha/app/controllers/api/v1/groups_controller.rb
+++ reech_alpha/app/controllers/api/v1/groups_controller.rb
@@ -4,44 +4,7 @@ module Api
     class GroupsController < ApiController
       before_filter :restrict_access 
       respond_to :json
-      # GET /groups
-      # GET /groups.json
-      def index
-        @groups = Group.all
-    
-        respond_to do |format|
-          format.html # index.html.erb
-          format.json { render json: @groups }
-        end
-      end
-    
-      # GET /groups/1
-      # GET /groups/1.json
-      def show
-        @group = Group.find(params[:id])
-    
-        respond_to do |format|
-          format.html # show.html.erb
-          format.json { render json: @group }
-        end
-      end
-    
-      # GET /groups/new
-      # GET /groups/new.json
-      def new
-        @group = Group.new
-    
-        respond_to do |format|
-          format.html # new.html.erb
-          format.json { render json: @group }
-        end
-      end
-    
-      # GET /groups/1/edit
-      def edit
-        @group = Group.find(params[:id])
-      end
-    
+      
       # POST /groups
       # POST /groups.json
       def create
@@ -134,33 +97,7 @@ end
          render :json => msg   
       end
     
-      # PUT /groups/1
-      # PUT /groups/1.json
-      def update
-        @group = Group.find(params[:id])
-    
-        respond_to do |format|
-          if @group.update_attributes(params[:group])
-            format.html { redirect_to @group, notice: 'Group was successfully updated.' }
-            format.json { head :no_content }
-          else
-            format.html { render action: "edit" }
-            format.json { render json: @group.errors, status: :unprocessable_entity }
-          end
-        end
-      end
-    
-      # DELETE /groups/1
-      # DELETE /groups/1.json
-      def destroy
-        @group = Group.find(params[:id])
-        @group.destroy
-    
-        respond_to do |format|
-          format.html { redirect_to groups_url }
-          format.json { head :no_content }
-        end
-      end
+      
     end
    
     

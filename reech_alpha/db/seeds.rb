4fd2b956570f093cd1fa05ffbb7abb3cd66f8995
zayed <zayed@heresy.in>
2014-09-01 12:40:04 +0530

    moving the index action from the migrations to the seed

--- reech_alpha/db/seeds.rb
+++ reech_alpha/db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
+
+# CreateCategories 
+Category.create(:title => "News")
+Category.create(:title => "Sports")
+Category.create(:title => "Entertainment")
+
+# InsertCategoryTocategoryMasterTable
+Category.create([{:title => "Arts & Culture"},
+    {:title => "Community Events"},
+    {:title => "Education"},
+    {:title => "Family & Pets"},
+    {:title => "Food & Dining"},
+    {:title => "Health"},
+    {:title => "Home Improvement"},
+    {:title => "Personal Services"},
+    {:title => "Professional Services"},
+    {:title => "Real Estate"},
+    {:title => "Arts & Culture"},
+    {:title => "Sports & Rec"},
+    {:title => "Technology"},
+    {:title => "Other"}])
